01930db7e5905c52ce4570764c02a990598dea87
Natalia Ozdemir <azolka@gmail.com>
2025-10-30 18:18:12 +0300

    tests are done

--- tests/test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://weathershopper.pythonanywhere.com/moisturizer');
-  await page.getByRole('button', { name: 'Add' }).nth(5).click();
-  await page.getByRole('button', { name: 'Add' }).nth(4).click();
-});
